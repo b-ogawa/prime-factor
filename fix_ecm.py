104d9fb189a10a585e4b3ea67a6611932c97e140
b-ogawa <xx.pankuzu.xx@gmail.com>
2026-06-10 04:21:01 +0900

    Delete fix_ecm.py

--- fix_ecm.py
+++
@@ -1,5 +0,0 @@
-with open('wasm_engine/src/ecm.rs', 'r') as f:
-    content = f.read()
-content = content.replace('fn ext_gcd_inverse_internal(', 'pub(crate) fn ext_gcd_inverse_internal(')
-with open('wasm_engine/src/ecm.rs', 'w') as f:
-    f.write(content)
